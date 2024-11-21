811fd02e757c367c27c68defc4130f659f071f92
SmirnovAntonZP <voodoo6991@gmail.com>
2024-11-21 20:08:51 +0300

    Fix code

--- task1.py
+++ task1.py
@@ -1,9 +1,3 @@
-# 3. Сценарий Foursquare
-# 4. Напишите сценарий на языке Python, который предложит пользователю ввести интересующую его категорию (например, кофейни, музеи, парки и т.д.).
-# 5. Используйте API Foursquare для поиска заведений в указанной категории.
-# 6. Получите название заведения, его адрес и рейтинг для каждого из них.
-# 7. Скрипт должен вывести название и адрес и рейтинг каждого заведения в консоль.
-
 import json
 import requests
 
@@ -30,8 +24,53 @@ if response.status_code == 200:
     data = json.loads(response.text)
     items = data["results"]
     for item in sorted(items, key = lambda item: item['name']):
-        print(f"{item['name']}, адрес: {item['location'].get('formatted_address', 'не определен') \
-        if item.get('location') else 'нет адреса'}, рейтинг {item.get('rating','не определен')}")
+        print(f"{item['name']}, адрес: {item['location'].get('formatted_address', 'не определен') if item.get('location') else 'нет адреса'}, рейтинг {item.get('rating','не определен')}")
 else:
     print("Запрос завершился ошибкой")
     print(response.status_code)
+#
+# import requests
+# import json
+#
+# # Ваши учетные данные API
+# CLIENT_ID = 'ZBTGA05DZIG1DKVHVULXWVCPACPKEOTBBL0CABLXGHX5QICL'
+# CLIENT_SECRET = 'QXQAD4J00VVDQS2TOTGW1R4CINUPFPHRWWH5BSQCBMHBA40C'
+#
+# # # Конечная точка API
+# endpoint = "https://api.foursquare.com/v3/places/search"
+#
+# # Определение параметров для запроса API
+# city = input("Введите название города: ")
+# category = input("Введите наименование категории: ")
+#
+# params = {
+#     "client_id": CLIENT_ID,
+#     "client_secret": CLIENT_SECRET,
+#     "near": city,
+#     "query": category
+# }
+#
+# headers = {
+#     "Accept": "application/json",
+#     "Authorization": "fsq3X4ThjIBPvSGUlebP8R4CeQ5bdrGL/9gLVqlWzuCJmk8="
+# }
+#
+# # Отправка запроса API и получение ответа
+# response = requests.get(endpoint, params=params, headers=headers)
+#
+# # Проверка успешности запроса API
+# if response.status_code == 200:
+#     print("Успешный запрос. Вот что нашлось по вашему запосу:")
+#     data = json.loads(response.text)
+#     venues = data["results"]
+#     for venue in venues:
+#         print("Название:", venue["name"])
+#         print("Адрес:", venue["location"]["address"])
+#         print("\n")
+# else:
+#     print("Запрос неудадся с кодом состояния:", response.status_code)
+#     print(response.text)
+#
+# # тренировка записи полученных данных в файл
+# with open('response_data.json', 'w') as f:
+#     json.dump(data, f)
